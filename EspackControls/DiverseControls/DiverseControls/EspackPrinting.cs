47cf2aecfa1698a54588f000a5a2e544560fe44c
David Vallés Galve <dvalles@espackeuro.com>
2018-03-14 17:18:56 +0100

    20180314 dvalles 03

---
+++ EspackControls/DiverseControls/DiverseControls/EspackPrinting.cs
@@ -0,0 +1,43 @@
+﻿using AccesoDatosNet;
+using EspackControls;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EspackControls;
+
+
+namespace DiverseControls
+{
+    public class EspackPrinting : PrintDocument
+    {
+        public string SQL { get; set; }
+        //public string[] Titles { get; set; }
+
+        List<EspackControl> Titles { get; set; }
+
+        public EspackPrinting( StaticRS RS) : base()
+        {
+            
+        }
+
+        public EspackPrinting( EspackControl VS ) : base()
+        {
+
+        }
+
+        protected override void OnPrintPage(PrintPageEventArgs e)
+        {
+            var g = e.Graphics;
+            g.PageUnit = GraphicsUnit.Millimeter;
+
+//            e.HasMorePages = (BodyList.LastPrintedLine < BodyList.Lines.Count);
+
+            base.OnPrintPage(e);
+        }
+
+    }
+}
