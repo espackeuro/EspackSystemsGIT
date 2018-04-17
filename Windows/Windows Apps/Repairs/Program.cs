09d247c07fe752618624312de91401848ca8604d
David Vallés Galve <dvalles@espackeuro.com>
2018-04-17 13:02:51 +0200

    20180417 dvalles 01

--- Windows/Windows Apps/Repairs/Program.cs
+++ Windows/Windows Apps/Repairs/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using CommonToolsWin;
 
 namespace Repairs
 {
@@ -12,11 +10,12 @@ namespace Repairs
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new fMainRepairs());
+            new frmSplash(args).ShowDialog();
+            Application.Run(new fMainRepairs(args));
         }
     }
 }
--- Windows/Windows Apps/Repairs/fMainRepairs.cs
+++ Windows/Windows Apps/Repairs/fMainRepairs.cs
@@ -1,16 +1,57 @@
 ﻿using System.Windows.Forms;
 using System.Reflection;
 using System;
+using AccesoDatosNet;
+using CommonTools;
+using CommonToolsWin;
 
 namespace Repairs
 {
     public partial class fMainRepairs : Form
     {
-        public fMainRepairs()
+        public fMainRepairs(string[] args)
         {
-            //var a = Type.GetType("");
             InitializeComponent();
+
+            var espackArgs = CT.LoadVars(args);
+            //Values.gDatos.DataBase = "Sistemas";//espackArgs.DataBase;
+            //Values.gDatos.Server = "192.168.200.7";//espackArgs.Server;
+            //Values.gDatos.User = "sa";//espackArgs.User;
+            //Values.gDatos.Password = "5380"; //espackArgs.Password;
+            Values.ProjectName = "Repairs";//Assembly.GetCallingAssembly().GetName().Name;
+            Values.gDatos.DataBase = espackArgs.DataBase;
+            Values.gDatos.Server = espackArgs.Server;
+            Values.gDatos.User = espackArgs.User;
+            Values.gDatos.Password = espackArgs.Password;
+
+            this.Text = string.Format("{0} Build {1} - ({2:yyyyMMdd})*", Values.ProjectName, Assembly.GetExecutingAssembly().GetName().Version.ToString(), CT.GetBuildDateTime(Assembly.GetExecutingAssembly()));
+            try
+            {
+                Values.gDatos.Connect();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error connecting to database: " + e.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+            Values.gDatos.Close();
+            //check settings file
+            if (!cSettings.SettingFileNameExists)
+            {
+                fSettings fSettings = new fSettings();
+                fSettings.ShowDialog();
+            }
+            Values.LabelPrinterAddress = cSettings.readSetting("labelPrinter");
+            Values.COD3 = cSettings.readSetting("COD3");
         }
 
+    }
+
+    public static class Values
+    {
+        public static cAccesoDatosNet gDatos = new cAccesoDatosNet();
+        public static string LabelPrinterAddress = "";
+        public static string COD3 = "";
+        public static string ProjectName = "";
     }
 }
