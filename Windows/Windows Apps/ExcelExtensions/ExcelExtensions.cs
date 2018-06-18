1ab576dd1ae117ae451d96dba1c0fdb2118badcd
RafaEspack <restelles@espackeuro.com>
2018-06-18 15:31:08 +0200

    20180618 2 restelles

---
+++ Windows/Windows Apps/ExcelExtensions/ExcelExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccesoDatos;
+namespace ExcelExtensions
+{
+    public static class CExcelExtensions
+    {
+        public static bool CreateExcelFile(this RSFrame rs)
+        {
+            return true;
+        }
+    }
+}
