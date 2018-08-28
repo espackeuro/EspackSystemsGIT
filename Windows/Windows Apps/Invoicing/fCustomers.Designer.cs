20d5e81288bfbe189813f660829f26696236605e
David Vallés Galve <dvalles@espackeuro.com>
2018-08-28 17:15:28 +0200

    20180828 dvalles 02

--- Windows/Windows Apps/Invoicing/fCustomers.Designer.cs
+++ Windows/Windows Apps/Invoicing/fCustomers.Designer.cs
@@ -544,7 +544,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(767, 476);
+            this.ClientSize = new System.Drawing.Size(1208, 550);
             this.Controls.Add(this.txtVAT);
             this.Controls.Add(this.txtDueDays);
             this.Controls.Add(this.txtCode);
