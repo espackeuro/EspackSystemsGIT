8f703ca354abc02c76e77021d5c3c172e9b2bba7
Rafael Estellés <restelles@grupointerpack.com>
2020-12-02 12:45:42 +0100

    20201202 restelles

---
+++ Windows/Windows Apps/DealisPickPack/fMain.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace DealisPickPack
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.IsMdiContainer = true;
+            this.Name = "fMain";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
---
+++ Windows/Windows Apps/DealisPickPack/fMain.cs
@@ -0,0 +1,41 @@
+﻿using AccesoDatosNet;
+using CommonTools;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DealisPickPack
+{
+    public partial class fMain : Form
+    {
+        public fMain(string[] args)
+        {
+            InitializeComponent();
+            var espackArgs = CT.LoadVars(args);
+            Values.gDatos.DataBase = espackArgs.DataBase;
+            Values.gDatos.Server = espackArgs.Server;
+            Values.gDatos.User = espackArgs.User;
+            Values.gDatos.Password = espackArgs.Password;
+            try
+            {
+                Values.gDatos.Connect();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Error connecting to database: " + e.Message, "ERROR", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Application.Exit();
+            }
+        }
+
+    }
+    public static class Values
+    {
+        public static cAccesoDatosNet gDatos = new cAccesoDatosNet();
+    }
+}
