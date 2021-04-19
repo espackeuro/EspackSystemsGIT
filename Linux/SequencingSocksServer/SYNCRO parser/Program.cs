ff81cd9ad189f454fc80789793ab7fd9875720ad
Rafael Estellés <restelles@grupointerpack.com>
2021-04-19 17:03:56 +0200

    rej 20210419

---
+++ Linux/SequencingSocksServer/SYNCRO parser/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Net;
+using System.Text;
+using System.Data.SqlClient;
+using System.Globalization;
+
+namespace SYNCRO_parser
+{
+    class Program
+    {
+        public static string filePath { get; set; }
+        static void Main(string[] args)
+        {
+            filePath = @"D:\20210419-113021 - 73.txt";
+            string fileText = "";
+            fileText = File.ReadAllText(filePath);
+            Regex rx = new Regex("UNH.*?\xC", RegexOptions.Compiled | RegexOptions.Multiline);
+            Regex rxLin = new Regex(@"UNH\+0(\d{4})\+SYNCRO:2'MID\+0\1\+(\d{6}):(\d{4})'CDT\+.*:(.*)'CSG(?:.*):(.*)'SEQ\+(.*)\+(.*)\+(.*):(.*)'ARD.*'SDD\+(\d{6}):(\d{4}).*'SUB.*UNT.*\1", RegexOptions.Compiled | RegexOptions.Multiline);
+            // Find matches.
+            MatchCollection matches = rx.Matches(fileText);
+            foreach (var match in matches)
+            {
+                MatchCollection linMatches = rxLin.Matches(match.ToString());
+                var hostName = Dns.GetHostName();
+                var transNumber = linMatches[0].Groups[1].Value;
+                var transDate = linMatches[0].Groups[2].Value;
+                var transTime = linMatches[0].Groups[3].Value;
+                var supplierCode = linMatches[0].Groups[4].Value;
+                var plantCode = linMatches[0].Groups[5].Value;
+                var seqQualifier = linMatches[0].Groups[6].Value;
+                var seqNumber = linMatches[0].Groups[7].Value;
+                var vinNumber = linMatches[0].Groups[8].Value;
+                var eccsSeqNum = linMatches[0].Groups[9].Value;
+                var callInDate = linMatches[0].Groups[10].Value;
+                var callInTime = linMatches[0].Groups[11].Value;
+
+                DB.toDataBase(hostName, transNumber, DateTime.ParseExact($"{transDate}:{transTime}", "yyMMdd:HHmm", new CultureInfo("es-ES")),
+                    supplierCode, plantCode, seqQualifier, seqNumber, vinNumber, eccsSeqNum,
+                    DateTime.ParseExact($"{callInDate}:{callInTime}", "yyMMdd:HHmm", new CultureInfo("es-ES")));
+
+            }
+
+        }
+    }
+    public static class DB { 
+    
+        public static bool toDataBase(string hostName, string transNumber, DateTime transDateTime, string supplierCode, string plantCode, string seqQualifier, string seqNumber, string vinNumber, string eccsSeqNum, DateTime callInDateTime)
+        {
+            try
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+
+                builder.DataSource = "DB01B";
+                builder.UserID = "sa";
+                builder.Password = "5380";
+                builder.InitialCatalog = "TRANSMISIONES";
+
+                using SqlConnection connection = new SqlConnection(builder.ConnectionString);
+                connection.Open();
+                using SqlCommand sp = new SqlCommand("pAddSequencingOdetteSyncroStrings", connection) { CommandType = System.Data.CommandType.StoredProcedure };
+                SqlCommandBuilder.DeriveParameters(sp);
+                sp.Parameters["@msg"].Value = "";
+                sp.Parameters["@HostName"].Value = hostName;
+                sp.Parameters["@TransNumber"].Value = transNumber;
+                sp.Parameters["@TransDateTime"].Value = transDateTime;
+                sp.Parameters["@SupplierCode"].Value = supplierCode;
+                sp.Parameters["@DestinationPlant"].Value = plantCode;
+                sp.Parameters["@SeqQualifier"].Value = seqQualifier;
+                sp.Parameters["@SeqNr"].Value = seqNumber;
+                sp.Parameters["@VinNr"].Value = vinNumber;
+                sp.Parameters["@ECCSSeqNum"].Value = eccsSeqNum;
+                sp.Parameters["@CallInDateTime"].Value = callInDateTime;
+
+                sp.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error adding line : {e.Message}");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Linux/SequencingSocksServer/SequencingSocksServer/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace AsyncEchoServer
+{
+    public class AsyncEchoServer
+    {
+        private int _listeningPort;
+        private int _dayCount=0;
+        private string _day = "";
+        public AsyncEchoServer(int port)
+        {
+            _listeningPort = port;
+        }
+        ///<summary>
+        /// Start listening for connection
+        /// </summary>
+        public async void Start()
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList.FirstOrDefault(x => x.GetAddressBytes()[0] == 192);
+            IPEndPoint localEndPoint = new IPEndPoint(IPAddress.Any, _listeningPort);
+            TcpListener listener = new TcpListener(IPAddress.Any, _listeningPort);
+            listener.Start();
+            LogMessage("Server is running");
+            LogMessage("Listening on port " + _listeningPort);
+
+            while (true)
+            {
+                LogMessage("Waiting for connections...");
+                try
+                {
+                    var tcpClient = await listener.AcceptTcpClientAsync();
+                    HandleConnectionAsync(tcpClient);
+                }
+                catch (Exception exp)
+                {
+                    LogMessage(exp.ToString());
+                }
+
+            }
+
+        }
+        ///<summary>
+        /// Process Individual client
+        /// </summary>
+        ///
+        ///
+        private async void HandleConnectionAsync(TcpClient tcpClient)
+        {
+            string clientInfo = tcpClient.Client.RemoteEndPoint.ToString();
+            LogMessage(string.Format("Got connection request from {0}", clientInfo));
+           
+            try
+            {
+                using (var networkStream = tcpClient.GetStream())
+                using (var reader = new StreamReader(networkStream))
+                using (var writer = new StreamWriter(networkStream))
+                {
+                    writer.AutoFlush = true;
+                    while (true)
+                    {
+                        string day = DateTime.Now.ToString("dd");
+                        if (day != _day)
+                        {
+                            _day = day;
+                            _dayCount = 1;
+                            LogMessage("Starting day");
+                        }
+                        else
+                            _dayCount++;
+                        var dataFromServer = await reader.ReadLineAsync();
+                        if (string.IsNullOrEmpty(dataFromServer))
+                        {
+                            break;
+                        }
+                        LogMessage("RECEIVED:" + dataFromServer);
+                        await File.AppendAllTextAsync($"/media/seqFiles/{DateTime.Now.ToString("yyyyMMdd-HHmmss")} - {_dayCount.ToString("D2")}.txt", dataFromServer);
+                        LogMessage("End Reception");
+                    }
+                }
+            }
+            catch (Exception exp)
+            {
+                LogMessage(exp.Message);
+            }
+            finally
+            {
+                LogMessage(string.Format("Closing the client connection - {0}",
+                            clientInfo));
+                tcpClient.Close();
+            }
+
+        }
+        private void LogMessage(string message,
+                                [CallerMemberName]string callername = "")
+        {
+            //File.WriteAllText("/root/bin/test.txt", string.Format("[{0}] - Thread-{1}- {2}", callername, Thread.CurrentThread.ManagedThreadId, message));
+            System.Console.WriteLine("[{0}] - Thread-{1}- {2}",
+                    callername, Thread.CurrentThread.ManagedThreadId, message);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Values.ProjectName = Assembly.GetCallingAssembly().GetName().Name;
+            Console.WriteLine(string.Join('-', args));
+            int _listenerPort = Convert.ToInt32(args[0]);
+            AsyncEchoServer async = new AsyncEchoServer(_listenerPort);
+            async.Start();
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+    public static class Values
+    {
+        public static string LabelPrinterAddress = "";
+        public static string COD3 = "";
+        public static string ProjectName = "";
+    }
+}
