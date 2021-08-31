c8403ced46ee4936f075995fbfcdc5a859ca3653
David Vallés Galve <dvalles@espackeuro.com>
2021-08-31 15:57:36 +0200

    20210831 dvalles

--- Linux/SFTPConnectSample/SFTPConnectSample/Program.cs
+++ Linux/SFTPConnectSample/SFTPConnectSample/Program.cs
@@ -7,10 +7,13 @@ using System.Collections;
 using System.Windows.Forms;
 using System.IO;
 using Tamir.SharpSsh.jsch;
+using Renci.SshNet;
+
 namespace SFTPConnectSample
 {
     class Program
     {
+        /*
         static void Main(string[] args)
         {
 
@@ -25,6 +28,129 @@ namespace SFTPConnectSample
             sftp.Get("a.xml", @"D://Key//hello.txt");
             ArrayList Newres = sftp.GetFileList(".");
             sftp.Close();
+        }
+        */
+        static void Main(string[] args)
+        {
+
+            SftpClient _sftp=null;
+
+            // replaced by args
+            string uploadFile = @"D:\prueba.txt";
+            string uploadDir = @"/sapglobal/interfaces/FOE/TST/outbound/EWM/3PL/PickingOrders/ES12/archive/";
+
+            // Try conneciton
+            if (!Connect(ref _sftp))
+                return;
+
+            //Upload(ref _sftp, @"prueba.txt", @"D:\prueba.txt",uploadDir);
+            Download(ref _sftp, @"prueba.txt",uploadDir+"prueba.txt", @"D:\");
+            
+            _sftp.Disconnect();
+
+        }
+
+        static bool Connect(ref SftpClient sftp)
+        {
+
+            // replace by args
+            //PrivateKeyFile keyFile = new PrivateKeyFile(@"/etc/ssl/private/sap_ftp/tstprivate2.ppk", "*Rsakey21*");
+            PrivateKeyFile _keyFile = new PrivateKeyFile(@"D:\tstprivate2.ppk", "*Rsakey21*");
+            var _keyFiles = new[] { _keyFile };
+            
+            string _userName = "ftpmesp";
+            string _serverIP = "19.70.124.63";
+
+
+            // Try connection
+            try
+            {
+                var methods = new List<AuthenticationMethod>();
+                methods.Add(new PrivateKeyAuthenticationMethod(_userName, _keyFiles));
+                ConnectionInfo _con = new ConnectionInfo(_serverIP, 22, _userName, methods.ToArray());
+                sftp = new SftpClient(_con);
+                sftp.Connect();
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+
+            return true;
+        }
+
+        static bool Upload(ref SftpClient sftp, string FileName,string Source, string TargetDir)
+        {
+            string _stage="";
+            string _file = TargetDir + FileName;
+
+            try
+            {
+                //
+                _stage = "Checkings";
+                if (!File.Exists(Source))
+                    throw new Exception("Source file not found.");
+                if (sftp.Exists(_file))
+                    throw new Exception("Target file already exists.");
+
+                //
+                _stage = "Changing remote directory";
+                sftp.ChangeDirectory(TargetDir);
+
+                //
+                _stage = "Uploading file";
+                using (FileStream fs = new FileStream(Source, FileMode.Open))
+                {
+                    sftp.BufferSize = 4 * 1024;
+                    sftp.UploadFile(fs, Path.GetFileName(Source));
+                    //Console.WriteLine("Upload File: " + Path.GetFileName(uploadFile));
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"[{_stage}] {ex.Message}");
+                return false;
+            }
+            
+            // OK
+            return true;
+        }
+        static bool Download(ref SftpClient sftp, string FileName, string Source, string TargetDir)
+        {
+            string _stage = "";
+            string _file = TargetDir + FileName;
+
+            try
+            {
+                //
+                _stage = "Checkings";
+                if (!sftp.Exists(Source))
+                    throw new Exception("Source file not found.");
+                if (File.Exists(_file))
+                    throw new Exception("Target file already exists.");
+
+                //
+                _stage = "Changing remote directory";
+                sftp.ChangeDirectory(TargetDir);
+
+                //
+                _stage = "Uploading file";
+                using (FileStream fs = new FileStream(Source, FileMode.Open))
+                {
+                    sftp.BufferSize = 4 * 1024;
+                    sftp.DownloadFile(Path.GetFileName(Source),fs);
+                    //Console.WriteLine("Upload File: " + Path.GetFileName(uploadFile));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"[{_stage}] {ex.Message}");
+                return false;
+            }
+
+            // OK
+            return true;
         }
     }
 }
---
+++ Linux/SFTPPrivatePublicKeys/SFTPPrivatePublicKeys/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SFTPPrivatePublicKeys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
