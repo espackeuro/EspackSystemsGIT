a3e4a9d8d6918ea499194843ced6a75cc69d3fb8
David Vallés Galve <dvalles@espackeuro.com>
2022-03-14 09:59:45 +0100

    20220314 dvalles-01

--- Linux/ConsoleApps/PostMan/Program.cs
+++ Linux/ConsoleApps/PostMan/Program.cs
@@ -16,7 +16,7 @@ namespace PostMan
             pDebug = false;
 #endif
             string _myName = System.Reflection.Assembly.GetCallingAssembly().GetName().Name;
-            string _sendTo="", _subject="";
+            string _sendTo = "", _subject = "", _carrier = "", _destPath = "";
             string _stage = "";
 
             try
@@ -34,32 +34,41 @@ namespace PostMan
                 switch (_fileName.Substring(_fileName.IndexOf("_") + 1))
                 {
                     case "DEDMB":
-                        _subject = "Informe de líneas TW";
-                        _sendTo = "po_lineas_tw@grupointerpack.com";
+                        _carrier = "TW";
                         break;
                     case "AB3CA":
-                        _subject = "Informe de líneas XPO";
+                        _carrier = "ND";
                         _sendTo = "po_lineas_nd@grupointerpack.com";
                         break;
                     case "V9":
-                        _subject = "Informe de líneas V9";
-                        _sendTo = "po_lineas_v9@grupointerpack.com";
+                        _carrier = "V9";
                         break;
                     default:
                         throw new Exception($"File {_fileName} not recognized");
                 }
 
+                _subject = $"Informe de líneas {_carrier}";
+                _sendTo = "dvalles@espackeuro.com"; // $"po_lineas_{_carrier.ToLower()}@grupointerpack.com";
+                _destPath = $"/media/HISTORICOS/Transmisiones/SAP_REPORT_LINEAS_{_carrier}/";
+
                 //
                 _stage = "Connecting to email server";
                 ExchangeAttachments _email = new ExchangeAttachments();
-                _email.Connect("processes", "*seso69*");
+                if (!_email.Connect("processes", "*seso69*"))
+                    throw new Exception("Could not connect to email server.");
 
                 //
                 _stage = "Sending email";
-                _email.SendEmail(_sendTo, _subject, "", _fileName);
+                if (!_email.SendEmail(_sendTo, _subject, "", _fileName))
+                    throw new Exception("Could not send the email.");
 
+                //
                 _stage = "Disconnecting";
                 _email.Dispose();
+
+                //
+                _stage = "Moving file";
+                File.Move(_fileName, $"{_destPath}{DateTime.Now.ToString("yyyyMMdd")}.{Path.GetFileName(_fileName)}");
 
             }
             catch (Exception ex)
