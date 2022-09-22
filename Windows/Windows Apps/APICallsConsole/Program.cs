8235825f188031810f3ad5335edd44a938f5fd69
David Vallés Galve <dvalles@espackeuro.com>
2022-09-22 16:19:27 +0200

    20220922-XX dvalles Project renamed and modifications started

---
+++ Windows/Windows Apps/APICallsConsole/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Xml;
+
+namespace JaguarLandRover.SupplierBroadcast.Client
+{
+    class Program
+    {
+
+        void Main(string[] args)
+        {
+            string _stage = "";
+            cJLR _api = new cJLR("https://motersuppliermsgqa.jlrext.com/SupplierBroadCast", "ESPACK", "Jag@2022", "2c50fb8f-787f-4b56-b510-2767703aef1c");
+
+            try
+            {
+                //
+                _stage = "";
+                if (!_api.Connect())
+                    throw new Exception("Error unknown");
+
+
+                _api.GetMessages();
+
+
+				//
+				// Acknowledge the last message ID received from the messages payload...
+				//
+				// client.AcknowledgeLastMessageReceived(API_KEY, lastMessageID);
+				//
+
+                
+                XmlDocument xml = new XmlDocument();
+                xml.LoadXml(_api.Messages);
+                Console.WriteLine(_api.messages);
+                XmlNodeList elemlist = xml.GetElementsByTagName("message");
+                string result = elemlist[0].InnerXml;
+
+                Console.WriteLine($"{elemlist[0].Attributes.GetNamedItem("messageID").InnerXml}\n{result}");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"[{this.GetType().Name}/{System.Reflection.MethodBase.GetCurrentMethod().Name}#{_stage}] {ex.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Hit any key to exit...");
+                Console.ReadKey();
+            }
+        }
+
+    }
+}
---
+++ Windows/Windows Apps/APICallsConsole/cJLR.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Xml;
+
+namespace JaguarLandRover.SupplierBroadcast.Client
+{
+    class cJLR : IDisposable
+    {
+        public static string ServiceURL; //= "https://motersuppliermsgqa.jlrext.com/SupplierBroadCast";
+        public static string User; //="ESPACK";
+        public static string Password;// = "Jag@2022";
+        public static string APIKey; // = "2c50fb8f-787f-4b56-b510-2767703aef1c"; //"c7a4284c-2ddf-4b07-ab6a-b489fa8ba1d5";
+
+        private SupplierWebService.SupplierServiceContractClient Client;
+        public string Messages;
+
+        public cJLR(string serviceURL, string apiKey, string user, string password)
+        {
+            ServiceURL = serviceURL;
+            User = user;
+            Password = password;
+            APIKey = apiKey;
+        }
+
+        public bool GetMessages()
+        {
+            string _stage = "";
+            
+            try
+            {
+                _stage = "Getting messages";
+                Messages = Client.GetMessages(APIKey);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"[{this.GetType().Name}/{System.Reflection.MethodBase.GetCurrentMethod().Name}#{_stage}] {ex.Message}");
+            }
+            
+            
+            return true;
+        }
+
+        public bool RemoveTimestampFromSecurityBinding<TChannel>(ClientBase<TChannel> serviceClient) where TChannel : class
+        {
+            string _stage = "";
+            try
+            {
+                //
+                _stage = "Binding elements";
+                BindingElementCollection elements = serviceClient.Endpoint.Binding.CreateBindingElements();
+                SecurityBindingElement sbe = elements.Find<SecurityBindingElement>();
+
+                if (sbe != null)
+                {
+                    sbe.IncludeTimestamp = false;
+                }
+
+                //
+                _stage = "Creating custom binding";
+                serviceClient.Endpoint.Binding = new CustomBinding(elements);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"[{this.GetType().Name}/{System.Reflection.MethodBase.GetCurrentMethod().Name}#{_stage}] {ex.Message}");
+            }
+            return true;
+        }
+
+        public bool Connect()
+        {
+            string _stage = "";
+
+            try
+            {
+                //
+                _stage = "Checking";
+                if (Client != null)
+                    throw new Exception("Client already connected");
+
+                //
+                _stage = "Creating client";
+                Client = new SupplierWebService.SupplierServiceContractClient();
+                Client.Endpoint.Address = new EndpointAddress(ServiceURL);
+                Client.ClientCredentials.UserName.UserName = User;
+                Client.ClientCredentials.UserName.Password = Password;
+
+                //
+                _stage = "Binding client elements";
+                if (!RemoveTimestampFromSecurityBinding<SupplierWebService.ISupplierServiceContract>(Client))
+                    throw new Exception("Error unknown");
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"[{this.GetType().Name}/{System.Reflection.MethodBase.GetCurrentMethod().Name}#{_stage}] {ex.Message}");
+            }
+            return true;
+        }
+
+        public bool Disconect()
+        {
+            string _stage = "";
+
+            try
+            {
+                //
+                _stage = "Checking";
+                if (Client == null)
+                    throw new Exception("Client is not connected");
+
+                //
+                _stage = "";
+                Client.Close();
+                Client = null;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"[{this.GetType().Name}/{System.Reflection.MethodBase.GetCurrentMethod().Name}#{_stage}] {ex.Message}");
+            }
+            return true;
+        }
+        public void Dispose()
+        {
+            try
+            {
+                Disconect();
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
